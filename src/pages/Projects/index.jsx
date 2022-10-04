38e13cad058ad957b99288fdbdfc943a9a89f20b
Jack Dempsey <jackdempsey@hey.com>
2022-10-04 17:13:32 -0400

    Create grid on projects.jsx using react-grid-system

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom"
 import "./projects.css"
 import Footer from "../../components/footer"
 import Header from "../../components/header"
+import { Container, Row, Col } from 'react-grid-system';
+
 const Projects = () =>{
 return (
     <>
@@ -9,9 +10,28 @@ return (
     <main id="projects">
         <h1>Projects</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-         <Link to="/" className="link">
-         Home
-        </Link>
+        
+       
+<Container fluid>
+  <Row debug>
+    <Col debug>1 utav 1</Col>
+    <Col debug>2 utav 2</Col>d
+    <Col debug>3 utav 3</Col>
+  </Row>
+  <br />
+  <Row debug>
+    <Col debug>1 of 3</Col>
+    <Col debug>2 of 3</Col>
+    <Col debug>3 of 3</Col>
+  </Row>
+  <br />
+  <Row debug>
+    <Col debug>1 of 3</Col>
+    <Col debug>2 of 3</Col>
+    <Col debug>3 of 3</Col>
+  </Row>
+</Container>
+
     </main>
     <Footer />
     </>
