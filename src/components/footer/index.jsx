f205b92448907ea8128fee2b1169f6cd19b4496a
Jack Dempsey <jackdempsey@hey.com>
2022-10-04 18:11:38 -0400

    update to footer component, add info and improve semantics

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,8 +4,30 @@ const Footer = () => {
     return (
         <footer className="footer">
         <h1>My Portfolio</h1>
-            <p>Email:hi@contact.us</p>
-            <p>Phone:123-123-1234</p>      
+
+        <section className="contact" vocab="reactportfolio123.netlify.com"typeof="localbusiness">
+            <h2>
+                Contact Me:
+            </h2>
+
+            <address property="email">
+                <a href="mailto:hi@contact.us">hi@contact.us</a>
+            </address>
+
+            <p className="telephone">123-456-7890</p>
+            
+            <address property="address" typeof="PostalAddress">
+                 <p property="streetAddress">123 Main Street., Suite 234</p>
+                 <p>
+                    <span property="addressLocality">Centerville</span>
+                    <br />
+                    <span property="addressRegion">New Jersey</span>
+                    <br />
+                    <span property="postalCode">07760</span>
+                 </p>
+                 <p property="addressCountry">United States of America</p>
+            </address>
+        </section>    
      
         <p>&copy; 2022</p>
 
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -10,12 +10,10 @@ return (
     <main id="projects">
         <h1>Projects</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        
-       
 <Container fluid>
   <Row debug>
     <Col debug>1 utav 1</Col>
-    <Col debug>2 utav 2</Col>d
+    <Col debug>2 utav 2</Col>
     <Col debug>3 utav 3</Col>
   </Row>
   <br />
