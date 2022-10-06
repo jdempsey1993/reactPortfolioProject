57ed5cf7839551918a2a1e9ad8708eb30824055d
Jack Dempsey <jackdempsey@hey.com>
2022-10-05 20:54:42 -0400

    Update projects index.jsx to include placeholder image tags

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -12,21 +12,21 @@ return (
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 <Container fluid>
   <Row debug>
-    <Col debug>1 utav 1</Col>
-    <Col debug>2 utav 2</Col>
-    <Col debug>3 utav 3</Col>
+    <Col debug><img src="*"alt="Abstract"></img></Col>
+    <Col debug><img src="*"alt="Abstract"></img></Col>
+    <Col debug><img src="*"alt="Abstract"></img></Col>
   </Row>
   <br />
   <Row debug>
-    <Col debug>1 of 3</Col>
-    <Col debug>2 of 3</Col>
-    <Col debug>3 of 3</Col>
+    <Col debug><img src="*"alt="Abstract"></img></Col>
+    <Col debug><img src="*"alt="Abstract"></img></Col>
+    <Col debug><img src="*"alt="Abstract"></img></Col>
   </Row>
   <br />
   <Row debug>
-    <Col debug>1 of 3</Col>
-    <Col debug>2 of 3</Col>
-    <Col debug>3 of 3</Col>
+    <Col debug><img src="*"alt="Abstract"></img></Col>
+    <Col debug><img src="*"alt="Abstract"></img></Col>
+    <Col debug><img src="*"alt="Abstract"></img></Col>
   </Row>
 </Container>
 
