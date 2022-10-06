cebd44ef8cf67963cef35b5af89c18f0d46da2ff
Jack Dempsey <jackdempsey@hey.com>
2022-10-06 09:46:49 -0400

    add image thumbnail to Projects page
    Update image grid to div layout, add css to scale on hover

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -3,6 +3,10 @@ import Footer from "../../components/footer"
 import Header from "../../components/header"
 import { Container, Row, Col } from 'react-grid-system';
 
+//Images
+import project1thumbnail from '../../media/thumbnails150/imageOne150.jpeg' 
+import project2thumbnail from '../../media/thumbnails150/imageTwo150.jpeg' 
+import { Link } from "react-router-dom";
 const Projects = () =>{
 return (
     <>
@@ -10,25 +14,59 @@ return (
     <main id="projects">
         <h1>Projects</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-<Container fluid>
+
+  <div className="thumbnailBox">
+
+  <Link to="#" className="thumbnail"><img src={project1thumbnail}alt="Abstract 1"
+    height={150} width={150}/></Link>
+
+<Link to="#" className="thumbnail"><img src={project2thumbnail}alt="Abstract 2"
+    height={150} width={150}/></Link>
+
+<Link to="#" className="thumbnail"><img src={project1thumbnail}alt="Abstract 1"
+    height={150} width={150}/></Link>
+
+<Link to="#" className="thumbnail"><img src={project2thumbnail}alt="Abstract 2"
+    height={150} width={150}/></Link>  
+    
+<Link to="#" className="thumbnail"><img src={project1thumbnail}alt="Abstract 1"
+    height={150} width={150}/></Link>
+
+<Link to="#" className="thumbnail"><img src={project2thumbnail}alt="Abstract 2"
+    height={150} width={150}/></Link>
+
+  </div>
+
+{/* <Container fluid>
   <Row debug>
-    <Col debug><img src="*"alt="Abstract"></img></Col>
-    <Col debug><img src="*"alt="Abstract"></img></Col>
-    <Col debug><img src="*"alt="Abstract"></img></Col>
+  <Col debug className="gridCol"><Link to="#" className="thumbnail"><img src={project1thumbnail}alt="Abstract 1"
+    height={150} width={150}/></Link></Col>
+
+<Col debug><Link to="#" className="thumbnail"><img src={project2thumbnail}alt="Abstract 2"
+    height={150} width={150}/></Link></Col>
+
+<Col debug><Link to="#" className="thumbnail"><img src={project1thumbnail}alt="Abstract 1"
+    height={150} width={150}/></Link></Col>
   </Row>
-  <br />
+
   <Row debug>
-    <Col debug><img src="*"alt="Abstract"></img></Col>
-    <Col debug><img src="*"alt="Abstract"></img></Col>
-    <Col debug><img src="*"alt="Abstract"></img></Col>
-  </Row>
-  <br />
-  <Row debug>
-    <Col debug><img src="*"alt="Abstract"></img></Col>
-    <Col debug><img src="*"alt="Abstract"></img></Col>
-    <Col debug><img src="*"alt="Abstract"></img></Col>
-  </Row>
-</Container>
+   
+  <Col debug><Link to="#" className="thumbnail"><img src={project2thumbnail}alt="Abstract 2"
+    height={150} width={150}/></Link></Col>
+
+<Col debug><Link to="#" className="thumbnail"><img src={project1thumbnail}alt="Abstract 1"
+    height={150} width={150}/></Link></Col>
+
+<Col debug><Link to="#" className="thumbnail"><img src={project2thumbnail}alt="Abstract 2"
+    height={150} width={150}/></Link></Col>
+    </Row>
+
+
+</Container> */}
+
+
+
+
 
     </main>
     <Footer />
